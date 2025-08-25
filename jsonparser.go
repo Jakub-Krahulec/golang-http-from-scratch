177dcf328ad44fc108c34ab6a6844c71ff771330
Jakub Krahulec <krahulecj@icloud.com>
2025-08-26 00:45:56 +0200

    Added array tokenization in json parser

--- jsonparser.go
+++ jsonparser.go
@@ -25,11 +25,10 @@ type Token struct {
 type TokenizerState int
 
 const (
-	Normal             TokenizerState = 0
-	ReadingNumber      TokenizerState = 1
-	ReadingString      TokenizerState = 2
-	ReadingKeyword     TokenizerState = 3
-	ReadingInsideArray TokenizerState = 4
+	Normal         TokenizerState = 0
+	ReadingNumber  TokenizerState = 1
+	ReadingString  TokenizerState = 2
+	ReadingKeyword TokenizerState = 3
 )
 
 type Tokenizer struct {
@@ -37,8 +36,8 @@ type Tokenizer struct {
 	Tokens []Token
 }
 
-func testJsonTokenize() {
-	testJson := "{\"name\": \"Jakub\", \"age\": 25, \"isGod\": true}"
+func testTokenizer() {
+	testJson := "{\"name\": \"Jakub\", \"age\": 25, \"isGod\": true, \"numbers\": [1, 2, 3]}"
 	// testJson := "{\"name\": \"Jakub\", \"age\": 28}"
 
 	tokenizer := Tokenizer{State: Normal, Tokens: []Token{}}
@@ -75,13 +74,13 @@ func (t *Tokenizer) checkEmptyJson(s string) error {
 
 func (t *Tokenizer) checkStartingAndEndingChar(s string) error {
 	strLen := len(s)
-	if s[0] != '{' {
-		return errors.New("json must start with {")
+	firstChar := s[0]
+	lastChar := s[strLen-1]
+	if (firstChar == '{' && lastChar == '}') || (firstChar == '[' && lastChar == ']') {
+		return nil
+	} else {
+		return errors.New("Invalid json first char or last char [make this error better]")
 	}
-	if s[strLen-1] != '}' {
-		return errors.New("json must end with }")
-	}
-	return nil
 }
 
 func (t *Tokenizer) tokenizeInput(s string) error {
@@ -92,6 +91,7 @@ func (t *Tokenizer) tokenizeInput(s string) error {
 	stringBuffer := ""
 	numberBuffer := ""
 	keywordBuffer := ""
+	howManyArraysIn := 0
 
 	for _, char := range s {
 		currentCharAsString := string(char)
@@ -114,7 +114,7 @@ func (t *Tokenizer) tokenizeInput(s string) error {
 				} else {
 					numberBuffer += currentCharAsString
 				}
-			} else if currentCharAsString == "," || currentCharAsString == "}" {
+			} else if currentCharAsString == "," || currentCharAsString == "}" || currentCharAsString == "]" {
 				t.State = Normal
 				if strings.Contains(numberBuffer, ".") {
 					t.Tokens = append(t.Tokens, Token{Type: Double, Value: numberBuffer})
@@ -123,11 +123,14 @@ func (t *Tokenizer) tokenizeInput(s string) error {
 				}
 				numberBuffer = ""
 				t.Tokens = append(t.Tokens, Token{Type: Structural, Value: currentCharAsString})
+				if currentCharAsString == "]" {
+					howManyArraysIn -= 1
+				}
 			} else {
 				return errors.New("invalid character in number")
 			}
 		case ReadingKeyword:
-			if currentCharAsString == "," || currentCharAsString == "}" {
+			if currentCharAsString == "," || currentCharAsString == "}" || currentCharAsString == "]" {
 				keywordBuffer = strings.Trim(keywordBuffer, " ")
 				if slices.Contains(keywords, keywordBuffer) {
 					t.Tokens = append(t.Tokens, Token{Type: Keyword, Value: keywordBuffer})
@@ -138,6 +141,9 @@ func (t *Tokenizer) tokenizeInput(s string) error {
 				keywordBuffer = ""
 				t.State = Normal
 				t.Tokens = append(t.Tokens, Token{Type: Structural, Value: currentCharAsString})
+				if currentCharAsString == "]" {
+					howManyArraysIn -= 1
+				}
 			} else {
 				currentCharLowered := strings.ToLower(currentCharAsString)
 				keywordBuffer += currentCharLowered
@@ -156,13 +162,23 @@ func (t *Tokenizer) tokenizeInput(s string) error {
 			} else if currentCharLowered == "f" || currentCharLowered == "t" || currentCharLowered == "n" {
 				keywordBuffer += currentCharLowered
 				t.State = ReadingKeyword
+			} else if currentCharAsString == "[" {
+				t.Tokens = append(t.Tokens, Token{Type: Structural, Value: currentCharAsString})
+				howManyArraysIn += 1
+			} else if currentCharAsString == "]" {
+				if howManyArraysIn > 0 {
+					howManyArraysIn -= 1
+					t.Tokens = append(t.Tokens, Token{Type: Structural, Value: currentCharAsString})
+				} else {
+					return errors.New("there must be opening square bracket before opening")
+				}
 			} else {
 				return errors.New("invalid character " + string(char))
 			}
 		}
 	}
 
-	if t.State != Normal {
+	if t.State != Normal || howManyArraysIn > 0 {
 		// I might add better error with which state it ended
 		return errors.New("invalid json.")
 	}
